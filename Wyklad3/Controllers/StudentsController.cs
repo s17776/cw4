ef2f645626f526d90cc1f42e92bf9ee3f32e6bf7
Marek Palasek <s17776@pjwstk.edu.pl>
2020-04-19 17:41:32 +0200

    Dodaj pliki projektów.

---
+++ Wyklad3/Controllers/StudentsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Wyklad3.Models;
+using Wyklad3.Services;
+
+namespace Wyklad3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private IDbService _dbService;
+        
+        string conString = "Data Source=db-mssql;Initial Catalog = s17776; Integrated Security = True";
+
+        public StudentsController(IDbService service)
+        {
+            _dbService = service;
+        }
+
+        //2. QueryString
+        [HttpGet]
+        public IActionResult GetStudents([FromServices]IDbService service, [FromQuery]string orderBy)
+        {
+            var list = new List<StudentInfoDTO>();
+            using (SqlConnection con = new SqlConnection(conString)) 
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select s.FirstName, s.LastName, s.BirthDate, st.Name, e.Semester from Student s join Enrollment e on e.IdEnrollment = s.IdEnrollment join Studies st on st.IdStudy = e.IdStudy";
+ 
+                con.Open();
+
+                SqlDataReader dr = com.ExecuteReader();
+
+                while(dr.Read())
+                {
+                    var st = new StudentInfoDTO
+                    {
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        BirthDate = dr["BirthDate"].ToString(),
+                        Name = dr["Name"].ToString(),
+                        Semester = dr["Semester"].ToString()
+                        };
+                    list.Add(st);
+                }
+
+            }
+               return Ok(list) ;
+
+
+        }
+        //[FromRoute], [FromBody], [FromQuery]
+        //1. URL segment
+        [HttpGet("{indexNumber}")]
+        public IActionResult GetStudent(string indexNumber) //action method
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select s.FirstName, s.LastName, s.BirthDate, st.Name, e.Semester from Student s join Enrollment e on e.IdEnrollment = s.IdEnrollment join Studies st on st.IdStudy = e.IdStudy where indexnumber = @index";
+
+
+                SqlParameter par = new SqlParameter();
+                par.Value = indexNumber;
+                par.ParameterName = "index";
+                com.Parameters.Add(par);
+
+
+                con.Open();
+                var dr = com.ExecuteReader();
+                if(dr.Read())
+                {
+                    var st = new StudentInfoDTO();
+
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.Name = dr["Name"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+                    return Ok(st);
+
+                }
+                return NotFound();
+            }
+        }
+
+        //3. Body - cialo zadan
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody]Student student)
+        {
+            student.IndexNumber=$"s{new Random().Next(1, 20000)}";
+            //...
+            return Ok(student); //JSON
+        }
+
+
+    }
+}
